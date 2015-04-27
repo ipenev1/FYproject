5bca623e713ca1b5da85122050c4933ddc32bc7b
Ilia Penev <d3b3l1q@abv.bg>
2015-04-27 08:07:22 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FYproject::Application.config.secret_token = '2221ea14d1ee413b0311077124743b39e4590f9eb0363358534a8ff8f814e8f3de89f91e16eb48d37ad0298576d57c102dd3f17d988f9eb869176bc31146ede2'
